ee79dedb2ecb2e0520d0fc43d4d89995c51050ae
Andy Jones <3579815+wandering-andy@users.noreply.github.com>
2023-12-08 15:17:38 -0700

    Setup script, tests, .gitignore

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='csv2mdpages',
+    version='0.1.0',
+    py_modules=['csv2mdpages'],
+    install_requires=[
+        'Click',
+        'mdutils',
+    ],
+    entry_points={
+        'console_scripts': [
+            'csv2mdpages = csv2mdpages:convert',
+        ],
+    },
+)
