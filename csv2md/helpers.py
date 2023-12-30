66a1c28c98844a6949397c766dce7d6c8aa519d2
Andy Jones <3579815+wandering-andy@users.noreply.github.com>
2023-12-29 22:52:35 -0700

    Tests and refactoring. Still needs work.

--- csv2md/helpers.py
+++ csv2md/helpers.py
@@ -1,5 +1,14 @@
 def replace_spaces(text: str) -> str:
     """
-    Replaces spaces in a string with hyphens and removes dots.
+    Replaces spaces in the given text with hyphens and removes periods, converting the text to lowercase.
+
+    Args:
+        text (str): The input text to be processed.
+
+    Returns:
+        str: The processed text with spaces replaced by hyphens, periods removed, and converted to lowercase.
+
+    Raises:
+        None
     """
     return text.replace(" ", "-").replace(".", "").lower()
